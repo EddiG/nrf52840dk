11c7039ba6fd1110f3ac8e01a805bf3d64385576
Eduard Gilmutdinov <edgilmutdinov@gmail.com>
2022-10-28 22:59:39 +0500

    Setup Clocks in RTC example

--- src/bin/rtc.rs
+++ src/bin/rtc.rs
@@ -3,6 +3,7 @@
 
 use blesf as _;
 use nrf52840_hal::{
+    clocks::{self, Clocks},
     pac::{interrupt, Interrupt, Peripherals, NVIC},
     rtc::{Rtc, RtcInterrupt},
 };
@@ -20,6 +21,16 @@ fn main() -> ! {
     defmt::println!("RTC Example");
 
     let periph = Peripherals::take().unwrap();
+
+    // Setup clocks
+    let clocks = Clocks::new(periph.CLOCK);
+    let clocks = clocks.enable_ext_hfosc();
+    let clocks = clocks.set_lfclk_src_external(clocks::LfOscConfiguration::NoExternalNoBypass);
+    let clocks = clocks.start_lfclk();
+    let _clocks = clocks.enable_ext_hfosc();
+    defmt::debug!("Clocks configured");
+
+    // Setup RTC0 interrupts
     let mut rtc = Rtc::new(periph.RTC0, 0).unwrap();
     rtc.enable_interrupt(RtcInterrupt::Tick, None);
     rtc.enable_counter();
